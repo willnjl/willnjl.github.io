0671d525a44c4cf6822a568d18c2818eb20e6155
Will <will_leighton@outlook.com>
2020-10-22 09:09:11 +0100

    working

---
+++ carousels.js
@@ -0,0 +1,79 @@
+$("#slider").owlCarousel({
+  margin: 20,
+  loop: true,
+  center: true,
+  responsive: {
+    0: {
+      stagePadding: 30,
+      items: 1,
+    },
+    400: {
+      stagePadding: 45,
+      items: 1,
+    },
+    450: {
+      stagePadding: 55,
+      items: 1,
+    },
+    500: {
+      stagePadding: 27,
+      items: 2,
+      center: false,
+    },
+    600: {
+      center: false,
+      items: 2,
+      stagePadding: 40,
+    },
+    700: {
+      center: false,
+      items: 3,
+      stagePadding: 30,
+    },
+    1000: {
+      center: false,
+      items: 4,
+      stagePadding: 30,
+    },
+  },
+});
+
+$("#slider2").owlCarousel({
+  margin: 20,
+  loop: true,
+  center: true,
+  responsive: {
+    0: {
+      stagePadding: 30,
+      items: 1,
+    },
+    400: {
+      stagePadding: 45,
+      items: 1,
+    },
+    450: {
+      stagePadding: 55,
+      items: 1,
+    },
+    500: {
+      stagePadding: 27,
+      items: 2,
+      center: false,
+    },
+    600: {
+      center: false,
+      items: 2,
+      stagePadding: 40,
+    },
+    700: {
+      center: false,
+      items: 3,
+      stagePadding: 30,
+    },
+    1000: {
+      center: false,
+      items: 4,
+      stagePadding: 30,
+    },
+  },
+});
