b7c282027df5b1c9b8c4c6af5092e4c064795566
Will <will_leighton@outlook.com>
2020-10-22 14:26:08 +0100

    added iffe to typewriter script

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,19 @@
-var app = document.getElementById("hero_links");
+((d, w) => {
+  var app = document.getElementById("hero_links");
 
-var typewriter = new Typewriter(app, {
-  // loop: true,
-  autoStart: true,
-  // cursor: ".",
-});
+  var typewriter = new Typewriter(app, {
+    // loop: true,
+    autoStart: true,
+    // cursor: ".",
+  });
 
-typewriter
-  .typeString("Technology, \n")
-  .pauseFor(2500)
-  // .deleteAll()
-  .typeString("Performance, \n")
-  .pauseFor(2500)
-  // .deleteAll()
-  .typeString("Environment \n")
-  .start();
+  typewriter
+    .typeString("Technology, \n")
+    .pauseFor(2500)
+    // .deleteAll()
+    .typeString("Performance, \n")
+    .pauseFor(2500)
+    // .deleteAll()
+    .typeString("Environment \n")
+    .start();
+})(document, window);
