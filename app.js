9e53b38ab8b6536ef51af41642d208ccc16c8453
Will <will_leighton@outlook.com>
2020-11-03 11:49:12 +0000

    fade in observer working

--- app.js
+++ app.js
@@ -1,58 +1,32 @@
 ((d, w) => {
-  const mapRange = (value, start1, stop1, start2, stop2) => {
-    value = (value - start1) / (stop1 - start1);
-    return start2 + value * (stop2 - start2);
+  const faders = d.querySelectorAll(".fade-in");
+  const sliders = d.querySelectorAll(".slide-in");
+
+  const appearOptions = {
+    threshold: 0.2,
+    rootMargin: "0px",
   };
-  const ticker = d.getElementById("ticker");
-  const skillTitle = d.getElementById("title_skills");
-  const skillSlider = d.getElementById("skills");
-  const contact = d.getElementById("contact");
-  const contactTitle = d.getElementById("title_contact");
-  const workTitle = d.getElementById("title_work");
-  const work = d.getElementById("work");
 
-  w.addEventListener("scroll", () => {
-    let y = w.pageYOffset;
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
 
-    //links
-    ticker.style.opacity = mapRange(y, 30, 300, 1, 0);
-
-    contactTitle.style.opacity = mapRange(y, 240, 428, 1, 0);
-
-    //first slider
-    if (y < 600) {
-      contact.style.opacity = mapRange(y, 240, 600, 0, 1);
-    } else {
-      contact.style.opacity = mapRange(y, 1250, 1300, 1, 0);
-    }
-
-    //contact
-    skillTitle.style.opacity = mapRange(y, 1000, 1300, 1, 0);
-    skillSlider.style.opacity = mapRange(y, 1000, 1500, 0, 1);
-    if (y > 2000) {
-      skillSlider.style.opacity = mapRange(y, 2100, 2200, 1, 0);
-    }
-
-    workTitle.style.opacity = mapRange(y, 1700, 2100, 1, 0);
-    work.style.opacity = mapRange(y, 1800, 2200, 0, 1);
+  faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
   });
 
-  contactTitle.addEventListener("click", () => {
-    contact.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-  skillTitle.addEventListener("click", () => {
-    skillSlider.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-  workTitle.addEventListener("click", () => {
-    work.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+  sliders.forEach((slider) => {
+    appearOnScroll.observe(slider);
   });
 })(document, window);
