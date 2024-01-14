2261544334b9a21eae59953eb1997d44cb4027b0
Will Leighton <will@fifteenten.co.uk>
2024-01-14 23:18:09 +0000

    use prod url

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:1337/api";
+const production = "https://porfolio-backend-l5ia7.ondigitalocean.app";
+const dev = "http://localhost:1337/api";
+
+export const BASE_URL =
+	process.env.NODE_ENV === "development" ? dev : production;
