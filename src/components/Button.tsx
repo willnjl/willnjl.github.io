71ab45625448f7b7d83aac8416eb100c432f0fcc
Will Leighton <will@fifteenten.co.uk>
2024-01-21 21:45:00 +0000

    create global button class

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+	text: string;
+	href?: string;
+}
+
+const Button: React.FC<Props> = (props: Props) => {
+	if (props.href) {
+		return (
+			<Link href={props.href} className="btn-primary">
+				{props.text}
+			</Link>
+		);
+	}
+
+	return <button className="btn-primary">{props.text}</button>;
+};
+
+export default Button;
