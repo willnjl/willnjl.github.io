f7c4c9167df2e7254acd840380ea584f9c76cbfe
Will <will@lunar.build>
2025-11-11 17:21:30 +0000

    close on escape

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import React from "react";
 import "./styles/app.scss";
 import Scene from "./components/Scene";
 export default function App() {
+	const [isActive, setIsActive] = React.useState(false);
+
+	React.useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "Escape") {
+				setIsActive(true);
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+		return () => window.removeEventListener("keydown", handleKeyDown);
+	}, []);
+
 	return (
-		<header className="coverpage">
+		<header className={`coverpage ${isActive ? "coverpage--closed" : ""}`}>
 			<Scene />
 			<div className="wrap">
 				<h2>React Widget</h2>
