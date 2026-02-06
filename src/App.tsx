a1bd903b191a09b91659ecf137a5c5530ecf2daf
Will <will@lunar.build>
2026-02-06 16:23:01 +0000

    WIP underwater

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./styles/app.scss";
 import Scene from "./components/Scene";
 import { useAppContext } from "./context/AppContext";
-import Content from "./components/Content";
+import EnterMessage from "./components/EnterMessage";
 import ScreenMoveIndicator from "./components/ScreenMoveIndicator";
+import BlurLayer from "./components/BlurLayer";
+import Content from "./components/Content";
+import Coverpage from "./components/Coverpage";
 
 export default function App() {
 	const { isClosed, isMobile } = useAppContext();
@@ -14,14 +17,9 @@ export default function App() {
 	}, [isClosed]);
 
 	return (
-		<header className={`coverpage ${isClosed ? "coverpage--closed" : ""}`}>
-			{!isClosed && (
-				<>
-					<ScreenMoveIndicator />
-					<Scene />
-					<Content />
-				</>
-			)}
-		</header>
+		<div className={`${isClosed ? "site--entered" : ""}`}>
+			<Coverpage />
+			<Content />
+		</div>
 	);
 }
---
+++ src/components/BlurLayer/index.tsx
@@ -0,0 +1,7 @@
+import "./BlurLayer.scss";
+
+const BlurLayer = () => {
+	return <div className="blur-layer"></div>;
+};
+
+export default BlurLayer;
