f55564207ca8370777235ac52749412d8c5d6766
Will <62480075+willnjl@users.noreply.github.com>
2026-03-10 21:31:04 +0000

    add nav buttons to projects

--- src/components/swiper.ts
+++ src/components/swiper.ts
@@ -1,9 +1,15 @@
 import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
 
 [].forEach.call(
 	document.querySelectorAll(".swiper-work"),
 	(item: HTMLElement) => {
 		const swiper = new Swiper(item, {
+			modules: [Navigation],
+			navigation: {
+				nextEl: ".swiper-nav-next",
+				prevEl: ".swiper-nav-prev",
+			},
 			slidesPerView: "auto",
 			spaceBetween: 4,
 			breakpoints: {
